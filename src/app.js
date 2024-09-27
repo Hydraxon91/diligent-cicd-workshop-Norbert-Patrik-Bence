26a5de4b34a7cc024189208b7eb97f5f1effea27
Bundula Patrik <bundulappp@gmail.com>
2024-09-27 09:33:28 +0200

    add new find by id case to create app method

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { list, formatList, format, add } from './todo.js';
+import { list, formatList, format, add, findById } from './todo.js';
 import { display } from './display.js';
 import { AppError } from './app-error.js';
 import { validateAddParams } from './validate.js';
@@ -8,18 +8,19 @@ export function createApp(todoStore, args) {
 
   switch (command) {
     case 'list':
-      const todos = list(todoStore)
-      display([
-        ...formatList(todos), 
-        `You have ${todos.length} todos.`
-      ]);
+      const todos = list(todoStore);
+      display([...formatList(todos), `You have ${todos.length} todos.`]);
       break;
     case 'add':
       const validated = validateAddParams(params);
       const added = add(todoStore, validated);
-      display(['New Todo added:', format(added)])
+      display(['New Todo added:', format(added)]);
+      break;
+    case 'findById':
+      const matchingTodoItem = findById(todoStore, params);
+      display(['Find by id: ', format(matchingTodoItem)]);
       break;
     default:
-      throw new AppError(`Unknown command: ${command}`)
+      throw new AppError(`Unknown command: ${command}`);
   }
 }
